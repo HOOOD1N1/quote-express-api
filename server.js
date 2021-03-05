e2d2c99743ce54421e7b2b10eca7f396338c5c4e
HOOOD1N1 <44617359+HOOOD1N1@users.noreply.github.com>
2021-03-05 11:43:23 +0200

    Added post and get/random paths

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { query } = require('express');
 const express = require('express');
 const app = express();
 
@@ -8,3 +9,42 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+
+app.get('/api/quotes/random', (req, res) => {
+    const quote = getRandomElement(quotes);
+    console.log(quote);
+    res.send({
+        quote
+    });
+
+})
+
+app.get('/api/quotes', (req, res) => {
+    if (!req.query) {
+        res.status(200).send(quotes);
+    } else {
+        if (req.query.person) {
+            const quotesString = [];
+            for (let quote in quotes) {
+                if (quote[person] === req.query.person) {
+                    quotesString.push(quote);
+                }
+            }
+            console.log({ quotes: quotesString })
+            res.send({ quotes: quotesString });
+        }
+    }
+})
+
+app.post('/api/quotes', (req, res) => {
+    if (req.query.person && req.query.quote) {
+        quotes.push({ quote: req.query.quote, person: req.query.person, });
+        res.status(200).send({ quote: quotes });
+    } else {
+        res.status(400).send();
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`server is listening at port ${PORT}`);
+})
